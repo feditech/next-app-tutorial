6b61d47d4db6510a755e54f0d3df97a0f4130a2c
feditech <82119072+feditech@users.noreply.github.com>
2022-06-02 14:45:56 +0500

    layout done

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div >
-      Welcome to next
+      <Head>
+        <title>Next Js news</title>
+        <meta name='keywords' content='web development, nextjs project' />
+      </Head>
+      <h1>Welcome to Next Js</h1>
      </div>
   )
 }
